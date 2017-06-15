3f13d57af91e38199230c7403cee21caa6a664ca
Florian <florian.vuillemot@epitech.eu>
2017-06-15 21:00:03 +0200

    Dependancies forget

--- my_horse_stable/mix.exs
+++ my_horse_stable/mix.exs
@@ -19,7 +19,7 @@ defmodule MyHorseStable.Mixfile do
   def application do
     [mod: {MyHorseStable, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :bamboo]]
+                    :phoenix_ecto, :postgrex, :bamboo, :timex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -39,7 +39,8 @@ defmodule MyHorseStable.Mixfile do
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:bamboo, "~> 0.7"},
-     {:bamboo_smtp, "~> 1.2.1"}]
+     {:bamboo_smtp, "~> 1.2.1"},
+     {:timex, "~> 3.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
